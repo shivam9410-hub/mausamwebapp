f3934bd551f98c7ac1111ea4d2c21bb4157dab3d
shivam khantwal <shivamkhantwal9410@gmail.com>
2023-04-23 09:09:34 +0530

    added loading

---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loading.css'
+const Loading = () => {
+  return (
+    <div className='loading'>
+      <h2>Loading...</h2>
+    </div>
+  )
+}
+
+export default Loading
--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,7 @@ import Forecast from './Forecast'
 import store from './redux/store'
 import { useFetcher } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import Loading from './Loading'
 
 const Main = () => {
     
@@ -21,10 +22,13 @@ const Main = () => {
  if(data.error===true){
   return <h1>No Such Place Found Please Try Again.</h1>
  }
+  if(data.isLoading===true){
+    return <Loading/>
+  }
   return (
     <div className='Main'>
  {
-  data.isLoading===true ?<h1 style={{color:'whitesmoke'}}>Loading...</h1> :(
+  (
      <>
       <Current data={data.placedata} ></Current>
       <Forecast data={data.placedata} ></Forecast>
